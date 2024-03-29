1ee16e2fa4f2567968f877b9317b084ef3e8d741
Ken <kenfoss0715@gmail.com>
2024-03-29 15:32:14 -0400

    backend with spring security 5

--- demo_backend/src/main/java/com/auth/demo/config/SecurityConfig.java
+++ demo_backend/src/main/java/com/auth/demo/config/SecurityConfig.java
@@ -1,24 +1,28 @@
 package com.auth.demo.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+
 @Configuration
 public class SecurityConfig  {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // set the name of the attribute the CsrfToken will be populated on
         http
+                .csrf((csrf) -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login/oauth2/code/google").permitAll()
-                        .anyRequest().fullyAuthenticated()
+                        .anyRequest().permitAll()
                 );
-//                .oauth2Login(withDefaults());
-//                .sessionManagement(session -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
+
 }
